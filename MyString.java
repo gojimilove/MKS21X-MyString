38c2060e9193c7e7a253351b5bfefe4d85ddb71b
Jessica Kim <jkim00@stuy.edu>
2018-11-21 09:47:59 -0500

    file created

---
+++ MyString.java
@@ -0,0 +1,21 @@
+public class MyString implements CharSequence {
+  public MyString() {
+
+  }
+
+  public char charAt(int index) {
+
+  }
+
+  public int length() {
+
+  }
+
+  public CharSequence subSequence(int start, int end) {
+
+  }
+
+  public String toString() {
+    
+  }
+}
