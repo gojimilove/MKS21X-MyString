365a304adeecceed30ebdbf24d0a3cf8913d53f9
Jessica Kim <jkim00@stuy.edu>
2018-11-26 20:51:28 -0500

    toString working

--- MyString.java
+++ MyString.java
@@ -8,7 +8,7 @@ public class MyString implements CharSequence,Comparable<CharSequence> {
     }
   }
 
-  public char charAt(int index) {
+  public char charAt(int index) throws ArrayIndexOutOfBoundsException{
     return data[index];
   }
 
@@ -17,8 +17,10 @@ public class MyString implements CharSequence,Comparable<CharSequence> {
   }
 
   public CharSequence subSequence(int start, int end) {
-    if (start == end) return "";
+    //initialize new charSequence
     CharSequence sub = "";
+
+    //add all chars in that range to the charSequence from the array
     for (int i = start; i < end; i++) {
     	sub += (""+data[i]);
     }
@@ -28,7 +30,7 @@ public class MyString implements CharSequence,Comparable<CharSequence> {
   public String toString() {
     String result = "";
     for (int i = 0; i < data.length; i++) {
-      
+      result+=data[i];
     }
     return result;
   }
@@ -40,9 +42,14 @@ public class MyString implements CharSequence,Comparable<CharSequence> {
   public static void main(String[]args) {
     MyString tester = new MyString("thisisatest");
     System.out.println(tester); //should print ""
-    System.out.println(tester.charAt(0)); //should be 't'
-    System.out.println(tester.charAt(9)); //should be 's'
+    try {
+    	System.out.println(tester.charAt(0)); //should be 't'
+    	System.out.println(tester.charAt(9)); //should be 's'
+    	System.out.println(tester.charAt(20)); //should be 's'
+    } catch (ArrayIndexOutOfBoundsException e) {
+    	System.out.println("TESTING");
+    }
     System.out.println(tester.length()); //should be 11
-    System.out.println(tester.subSequence(1, 4)); //should be "his"
+    System.out.println(tester.subSequence(4, -1)); //should be "his"
   }
 }
